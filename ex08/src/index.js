76e44d594e8d0ad8fb1952126609eaf7dacde8b4
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-30 11:58:42 +0400

    Adding ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,66 @@
+
+import { find } from 'lodash';
+
+var users = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "age": 24,
+        "gender": "male"
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "age": 5,
+        "gender": "female"
+    },
+    {
+        "firstName": "Jim",
+        "lastName": "Carrey",
+        "age": 54,
+        "gender": "male"
+    },
+    {
+        "firstName": "Kate",
+        "lastName": "Winslet",
+        "age": 50,
+        "gender": "female"
+    }
+
+];
+
+function getUsers() {
+    var output = " ";
+    for (var i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    return output;
+    }
+}
+
+
+function findUser(lastName, gender) {
+    try {
+
+        var users = find(users, { lastName, gender }); 
+        console.log(users);
+        var iFindUser = (username + is + lastname, iFindUser);
+
+        return iFindUser;
+
+    } catch (error) {
+
+        console.log("Cannot read property 'firstName' of undefined");
+        return "Cannot read property 'firstName' of undefined";
+        
+    }
+}
+
+getUsers();
+findUser("Doe", "male");
+findUser("Doe", "female");
+findUser("Carrey", "male");
+findUser("Winslet", "female");
+
+export default findUser;
+
+
