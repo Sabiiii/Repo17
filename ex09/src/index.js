48b5dda297ccd26d87697363b06b4afe4edcac32
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-30 12:19:31 +0400

    Adding find a user by id

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,17 +1,64 @@
+
+
 const _ = require('lodash');
 
+var users = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "age": 24,
+        "gender": "male"
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "age": 5,
+        "gender": "female"
+    },
+    {
+        "firstName": "Jim",
+        "lastName": "Carrey",
+        "age": 54,
+        "gender": "male"
+    },
+    {
+        "firstName": "Kate",
+        "lastName": "Winslet",
+        "age": 50,
+        "gender": "female"
+    }
 
+];
+
+function getUsers() {
+    var output = " ";
+    for (var i = 0; i < users.length; i++) {
+        console.log(users[i]);
+        return output;
+    }
+}
 
 function findUserById(id) {
     try {
 
+         var user = _.find(users, { 'id':'4' }); 
+            console.log(user);
+            var iFindUser = ('{id}' -'{firstName} + {lastName} + " is " + {age} + " , "  + {gender}');
+            console.log(user);
+            return iFindUser;
+            
+
     } catch (error) {
-        return 
-        console.log();
+        console.log("Cannot read property 'id'");
+        return "Cannot read property 'id'";
+        
     }
 }
 
+
 getUsers();
-findUserById();
+console.log(findUserById());
 
 module.exports = findUserById;
+
+
